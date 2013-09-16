a282b3dd90e86c6be083acdec638d9f6bc9fae90
Venkateswara VP <venkateswara.vp@gmail.com>
2013-09-15 23:28:25 -0400

    Quick fix

--- src/main/java/demo/domain/Customer.java
+++ src/main/java/demo/domain/Customer.java
@@ -1,115 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package demo.domain;
-
-import java.io.Serializable;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
-
-/**
- *
- * @author ctsuser1
- */
-@Entity
-@Table(name = "CUSTOMER")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c"),
-    @NamedQuery(name = "Customer.findById", query = "SELECT c FROM Customer c WHERE c.id = :id"),
-    @NamedQuery(name = "Customer.findByAge", query = "SELECT c FROM Customer c WHERE c.age = :age")})
-public class Customer implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "ID")
-    private Integer id;
-    @Basic(optional = false)
-    @NotNull
-    @Lob
-    @Size(min = 1, max = 65535)
-    @Column(name = "NAME")
-    private String name;
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "AGE")
-    private int age;
-
-    public Customer() {
-    }
-
-    public Customer(Integer id) {
-        this.id = id;
-    }
-
-    public Customer(Integer id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Customer)) {
-            return false;
-        }
-        Customer other = (Customer) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "demo.services.db.Customer[ id=" + id + " ]";
-    }
-    
-=======
 package demo.domain;
 
 import java.io.Serializable;
@@ -119,9 +7,9 @@ import org.codehaus.jettison.json.JSONStringer;
 import com.sun.xml.bind.v2.model.core.ID;
 
 /**
- * 
+ *
  * @author Venkateswara
- * 
+ *
  */
 public class Customer implements Serializable {
 
@@ -193,5 +81,4 @@ public class Customer implements Serializable {
 		builder.append("}");
 		return builder.toString();
 	}
->>>>>>> 80270f912fd4a84b7ca15f03eddbbc0965bc8c4b
 }
