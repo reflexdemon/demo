b72827878a8d572be681f8c4c10025bbe270fe83
Venkateswara Venkatraman <venkateswara.venkatraman@cbeyond.net>
2013-09-16 13:19:18 -0400

    Added comments

--- src/main/java/demo/services/F2C.java
+++ src/main/java/demo/services/F2C.java
@@ -5,6 +5,7 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.logging.Log;
@@ -12,45 +13,53 @@ import org.apache.commons.logging.LogFactory;
 import org.codehaus.jettison.json.JSONException;
 
 import demo.domain.DemoVO;
-
+/**
+ * This is a generic service that can be used to convert Fahrenheit value to
+ * celsius.
+ * 
+ * <pre>
+ * GET /services/f2c        This will assume the input is ZERO.
+ * GET /services/f2c/{f}    This will convert the passed Fahrenheit value to celsius
+ * </pre>
+ * 
+ * @author Venkateswara VP
+ */
 @Path("f2c")
 public class F2C {
-
-    /** The log. */
-    private static Log LOG = LogFactory.getLog(F2C.class.getName());
-    
-    @GET
-    @Produces("application/json")
-    @HeaderParam("Access-Control-Allow-Origin: *")
-    public Response convertFtoC() throws JSONException {
-
-	DemoVO jsonObject = new DemoVO();
-	float fahrenheit = 0.0f;
-	float celsius;
-	celsius = (fahrenheit - 32.0f) * 5.0f / 9.0f;
-	jsonObject.setFahrenheit(fahrenheit);
-	jsonObject.setCelsius(celsius);
-	LOG.debug(fahrenheit + "F --> " + celsius + "C");
-
-	return Response.status(200).entity(jsonObject).build();
-    }
-
-    @Path("{f}")
-    @GET
-    @Produces("application/json")
-    @HeaderParam("Access-Control-Allow-Origin: *")
-    public Response convertFtoCfromInput(@PathParam("f") float f)
-	    throws JSONException {
-
-	
-	//System.out.println("convertFtoCfromInput(" + f + ")");
-	DemoVO jsonObject = new DemoVO();
-	float celsius;
-	float fahrenheit = f;
-	celsius = (fahrenheit - 32.0f) * 5.0f / 9.0f;
-	jsonObject.setFahrenheit(fahrenheit);
-	jsonObject.setCelsius(celsius);
-	LOG.debug(fahrenheit + "F --> " + celsius + "C");
-	return Response.status(200).entity(jsonObject).build();
-    }
+   
+   /** The log. */
+   private static Log LOG = LogFactory.getLog(F2C.class.getName());
+   
+   @GET
+   @Produces(MediaType.APPLICATION_JSON)
+   @HeaderParam("Access-Control-Allow-Origin: *")
+   public Response convertFtoC() throws JSONException {
+      
+      final DemoVO jsonObject = new DemoVO();
+      final float fahrenheit = 0.0f;
+      float celsius;
+      celsius = (fahrenheit - 32.0f) * 5.0f / 9.0f;
+      jsonObject.setFahrenheit(fahrenheit);
+      jsonObject.setCelsius(celsius);
+      LOG.debug(fahrenheit + "F --> " + celsius + "C");
+      
+      return Response.status(200).entity(jsonObject).build();
+   }
+   
+   @Path("{f}")
+   @GET
+   @Produces(MediaType.APPLICATION_JSON)
+   @HeaderParam("Access-Control-Allow-Origin: *")
+   public Response convertFtoCfromInput(@PathParam("f") final float f)
+         throws JSONException {
+      
+      final DemoVO jsonObject = new DemoVO();
+      float celsius;
+      final float fahrenheit = f;
+      celsius = (fahrenheit - 32.0f) * 5.0f / 9.0f;
+      jsonObject.setFahrenheit(fahrenheit);
+      jsonObject.setCelsius(celsius);
+      LOG.debug(fahrenheit + "F --> " + celsius + "C");
+      return Response.status(200).entity(jsonObject).build();
+   }
 }
