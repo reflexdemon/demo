0ef2fbc82ae98d71e2a8c5cbf0ab234cd63c6952
Venkateswara Venkatraman <venkateswara.venkatraman@cbeyond.net>
2013-09-20 06:50:18 -0400

    Gots to d what I do!

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -3,9 +3,9 @@ var remoteServer = "./";
 //var remoteServer = "http://localhost:8080/demo/";
 window.HomeView = Backbone.View.extend({
 
-    template:_.template($('#home').html()),
+    template : _.template($('#home').html()),
 
-    render:function (eventName) {
+    render : function(eventName) {
         $(this.el).html(this.template());
         return this;
     }
@@ -13,9 +13,9 @@ window.HomeView = Backbone.View.extend({
 
 window.Page1View = Backbone.View.extend({
 
-    template:_.template($('#page1').html()),
+    template : _.template($('#page1').html()),
 
-    render:function (eventName) {
+    render : function(eventName) {
         $(this.el).html(this.template());
         return this;
     }
@@ -23,68 +23,67 @@ window.Page1View = Backbone.View.extend({
 
 window.Page2View = Backbone.View.extend({
 
-    template:_.template($('#page2').html()),
+    template : _.template($('#page2').html()),
 
-    render:function (eventName) {
+    render : function(eventName) {
         $(this.el).html(this.template());
         return this;
     }
 });
 
-
 window.Result1View = Backbone.View.extend({
 
-    template:_.template($('#result1').html()),
+    template : _.template($('#result1').html()),
 
-    render:function (eventName) {
+    render : function(eventName) {
         $(this.el).html(this.template());
         var fahernheitValue = parseFloat($("#fahrenheitTxt").val());
-		var urlStr = remoteServer + "services/f2c/" + fahernheitValue;
-		$.ajax({
-		  type: "GET",
-		  url: urlStr,
-		  dataType: "json"
-		}).done(function(data) {
-                        console.log("JSON Response:" + data);
-                        console.log("Celsius:" + data.celsius);
-			$("#output1").html(data.celsius);
-		});
+        var urlStr = remoteServer + "services/f2c/" + fahernheitValue;
+        $.ajax({
+            type : "GET",
+            url : urlStr,
+            dataType : "json"
+        }).done(function(data) {
+            console.log("JSON Response:" + data);
+            console.log("Celsius:" + data.celsius);
+            $("#output1").html(data.celsius);
+        });
         return this;
     }
 });
 
 window.Result2View = Backbone.View.extend({
 
-    template:_.template($('#result2').html()),
+    template : _.template($('#result2').html()),
 
-    render:function (eventName) {
+    render : function(eventName) {
         $(this.el).html(this.template());
         var celsiusValue = parseFloat($("#celsiusTxt").val());
-		var urlStr = remoteServer + "services/c2f/" + celsiusValue;
-		$.ajax({
-		  type: "GET",
-		  url: urlStr,
-		  dataType: "json"
-		}).done(function(data) {
-			console.log("JSON Response:" + data);
-                        console.log("Fahernheit:" + data.fahrenheit);
-			$("#output2").html(data.fahrenheit);
-		});
+        var urlStr = remoteServer + "services/c2f/" + celsiusValue;
+        $.ajax({
+            type : "GET",
+            url : urlStr,
+            dataType : "json"
+        }).done(function(data) {
+            console.log("JSON Response:" + data);
+            console.log("Fahernheit:" + data.fahrenheit);
+            $("#output2").html(data.fahrenheit);
+        });
         return this;
     }
 });
 
 var AppRouter = Backbone.Router.extend({
 
-    routes:{
-        "":"home",
-        "page1":"page1",
-        "page2":"page2",
-        "result1":"result1",
-        "result2":"result2"
+    routes : {
+        "" : "home",
+        "page1" : "page1",
+        "page2" : "page2",
+        "result1" : "result1",
+        "result2" : "result2"
     },
 
-    initialize:function () {
+    initialize : function() {
         // Handle back button throughout the application
         $('.back').live('click', function(event) {
             window.history.back();
@@ -93,32 +92,32 @@ var AppRouter = Backbone.Router.extend({
         this.firstPage = true;
     },
 
-    home:function () {
+    home : function() {
         console.log('#home');
         this.changePage(new HomeView());
     },
 
-    page1:function () {
+    page1 : function() {
         console.log('#page1');
         this.changePage(new Page1View());
     },
 
-    page2:function () {
+    page2 : function() {
         console.log('#page2');
         this.changePage(new Page2View());
     },
-            
-    result1:function () {
+
+    result1 : function() {
         console.log('#result1');
         this.changePage(new Result1View());
     },
-            
-    result2:function () {
+
+    result2 : function() {
         console.log('#result2');
         this.changePage(new Result2View());
     },
 
-    changePage:function (page) {
+    changePage : function(page) {
         $(page.el).attr('data-role', 'page');
         page.render();
         $('body').append($(page.el));
@@ -128,13 +127,15 @@ var AppRouter = Backbone.Router.extend({
             transition = 'none';
             this.firstPage = false;
         }
-        $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
+        $.mobile.changePage($(page.el), {
+            changeHash : false,
+            transition : transition
+        });
     }
-
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
     console.log('document ready');
     app = new AppRouter();
     Backbone.history.start();
-});
+}); 
